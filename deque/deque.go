19ba89d58a70e5b519f8da22b985a02a88fd41e9
psimaf <psimaf27@gmail.com>
2025-06-08 12:52:53 +0300

    add mutex for all function

--- deque/deque.go
+++ deque/deque.go
@@ -4,39 +4,57 @@ package deque
 
 import (
 	"container/list"
+	"sync"
 )
 
 // structure of a dequeue
-type Deque struct {
+type Deque[T any] struct {
 	list *list.List
+	mu   sync.RWMutex
 }
 
-func New() *Deque {
-	return &Deque{
+func New[T any]() *Deque[T] {
+	return &Deque[T]{
 		list: list.New(),
 	}
 }
 
+func zeroval[T any]() T {
+	var zero T
+	return zero
+}
+
 // Len returns the total length of all elements in the queue
-func (d *Deque) Len() int {
+func (d *Deque[T]) Len() int {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
 	return d.list.Len()
 }
 
 // IsEmpty returns a boolean value that signals that the list is empty
-func (d *Deque) IsEmpty() bool {
-	return d.Len() == 0
+func (d *Deque[T]) IsEmpty() bool {
+	d.mu.RLock()
+	defer d.mu.RUnlock()
+
+	return d.list.Len() == 0
 }
 
 // PushFront adds the listed values ​​to the front of the queue
-func (d *Deque) PushFront(values ...any) {
-	values = revers(values)
-	for _, v := range values {
-		d.list.PushFront(v)
+func (d *Deque[T]) PushFront(values ...T) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	for i := len(values) - 1; i >= 0; i-- {
+		d.list.PushFront(values[i])
 	}
 }
 
 // PushBack adds the listed values ​​to the end of the queue
-func (d *Deque) PushBack(values ...any) {
+func (d *Deque[T]) PushBack(values ...T) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
 	for _, v := range values {
 		d.list.PushBack(v)
 	}
@@ -44,39 +62,61 @@ func (d *Deque) PushBack(values ...any) {
 
 // PopFront retrieves and removes the first element from the queue, a
 // boolean value signals that the resulting value is not a zero value
-func (d *Deque) PopFront() (any, bool) {
-	if d.IsEmpty() {
-		return nil, false
+func (d *Deque[T]) PopFront() (T, bool) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.list.Len() == 0 {
+		return zeroval[T](), false
 	}
 
 	elem := d.list.Front()
 	defer d.list.Remove(elem)
 
-	return elem.Value, true
+	val, ok := elem.Value.(T)
+	if !ok {
+		return zeroval[T](), false
+	}
+
+	return val, true
 }
 
 // PopBack retrieves and removes the last element from the queue, a
 // boolean value signals that the resulting value is not a zero value
-func (d *Deque) PopBack() (any, bool) {
-	if d.IsEmpty() {
-		return nil, false
+func (d *Deque[T]) PopBack() (T, bool) {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.list.Len() == 0 {
+		return zeroval[T](), false
 	}
 
 	elem := d.list.Back()
 	defer d.list.Remove(elem)
 
-	return elem.Value, true
+	val, ok := elem.Value.(T)
+	if !ok {
+		return zeroval[T](), false
+	}
+
+	return val, true
 }
 
 // Clear removes all elements from queue
-func (d *Deque) Clear() int {
-	cleared := 0
-	if d.IsEmpty() {
-		return cleared
+func (d *Deque[T]) Clear() int {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.list.Len() == 0 {
+		return 0
 	}
 
-	for range d.Len() {
-		d.list.Remove(d.list.Front())
+	cleared := 0
+
+	for e := d.list.Front(); e != nil; {
+		next := e.Next()
+		d.list.Remove(e)
+		e = next
 		cleared++
 	}
 
@@ -84,35 +124,23 @@ func (d *Deque) Clear() int {
 }
 
 // ToArray converts all values ​​from a queue into an array
-func (d *Deque) ToArray() []any {
-	if d.IsEmpty() {
-		return []any{}
+func (d *Deque[T]) ToArray() []T {
+	d.mu.Lock()
+	defer d.mu.Unlock()
+
+	if d.list.Len() == 0 {
+		return []T{}
 	}
 
-	arr := make([]any, 0, d.Len())
+	arr := make([]T, 0, d.list.Len())
 
-	curr := d.list.Front()
-
-	for curr.Next() != nil {
-		arr = append(arr, curr.Value)
-		curr = curr.Next()
+	for e := d.list.Front(); e != nil; e = e.Next() {
+		val, ok := e.Value.(T)
+		if !ok {
+			break
+		}
+		arr = append(arr, val)
 	}
-
-	// Add last element value
-	arr = append(arr, curr.Value)
 
 	return arr
-}
-
-// reverse returns a reversed copy of the input slide
-func revers(a []any) []any {
-	if len(a) == 0 {
-		return a
-	}
-
-	res := a
-	for i, j := 0, len(res)-1; i < j; i, j = i+1, j-1 {
-		res[i], res[j] = res[j], res[i]
-	}
-	return res
 }
--- deque/deque_test.go
+++ deque/deque_test.go
@@ -5,85 +5,35 @@
 package deque
 
 import (
+	"container/list"
 	"reflect"
 	"testing"
 )
 
-func Test_revers(t *testing.T) {
-	tests := []struct {
-		name string
-		a    []any
-		want []any
-	}{
-		{
-			name: "base case",
-			a:    []any{1, 2, 3},
-			want: []any{3, 2, 1},
-		},
-
-		{
-			name: "empty input slice",
-			a:    []any{},
-			want: []any{},
-		},
-
-		{
-			name: "nil input slice",
-			a:    nil,
-			want: nil,
-		},
-
-		{
-			name: "\"palindrom\" input slice",
-			a:    []any{3, 0, 0, 3},
-			want: []any{3, 0, 0, 3},
-		},
-
-		{
-			name: "base-case",
-			a:    []any{1, '2', "3"},
-			want: []any{"3", '2', 1},
-		},
-
-		{
-			name: "mixed input slice",
-			a:    []any{3, "hello", complex64(3.43 * 23), uint(34), nil, '1'},
-			want: []any{'1', nil, uint(34), complex64(3.43 * 23), "hello", 3},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			if got := revers(tt.a); !reflect.DeepEqual(got, tt.want) {
-				t.Errorf("revers() = %v, want %v", got, tt.want)
-			}
-		})
-	}
-}
-
 func TestDeque_Len(t *testing.T) {
-	fullDeque := New()
-	fullDeque.PushFront(1, 2, 3, 4)
-
 	tests := []struct {
-		name string
-		d    *Deque
-		want int
+		name       string
+		initValues []any
+		want       int
 	}{
 		{
-			name: "empty deque",
-			d:    New(),
-			want: 0,
+			name:       "empty deque",
+			initValues: []any{},
+			want:       0,
 		},
 
 		{
-			name: "full deque",
-			d:    fullDeque,
-			want: 4,
+			name:       "full deque",
+			initValues: []any{1, 2, "Hello", false},
+			want:       4,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.d.Len(); got != tt.want {
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			if got := d.Len(); got != tt.want {
 				t.Errorf("Deque.Len() = %v, want %v", got, tt.want)
 			}
 		})
@@ -91,29 +41,29 @@ func TestDeque_Len(t *testing.T) {
 }
 
 func TestDeque_IsEmpty(t *testing.T) {
-	d := New()
-	d.PushFront(1)
-
 	tests := []struct {
-		name string
-		d    *Deque
-		want bool
+		name       string
+		initValues []any
+		want       bool
 	}{
 		{
-			name: "empty deque",
-			d:    New(),
-			want: true,
+			name:       "empty deque",
+			initValues: []any{},
+			want:       true,
 		},
 
 		{
-			name: "full deque",
-			d:    d,
-			want: false,
+			name:       "full deque",
+			initValues: []any{1, 2, 3, 4, 5},
+			want:       false,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.d.IsEmpty(); got != tt.want {
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			if got := d.IsEmpty(); got != tt.want {
 				t.Errorf("Deque.IsEmpty() = %v, want %v", got, tt.want)
 			}
 		})
@@ -121,40 +71,35 @@ func TestDeque_IsEmpty(t *testing.T) {
 }
 
 func TestDeque_ToArray(t *testing.T) {
-	mixed := New()
-	mixedValues := []any{1, uint(2), 0.57, 4, 5 * 2i, false, 7, true, "9", int16(10)}
-	mixed.PushBack(mixedValues...)
-
-	basic := New()
-	values := []any{1, "hello"}
-	basic.PushBack(values...)
-
 	tests := []struct {
-		name string
-		d    *Deque
-		want []any
+		name       string
+		initValues []any
+		want       []any
 	}{
 		{
-			name: "empty deque",
-			d:    New(),
-			want: []any{},
+			name:       "empty deque",
+			initValues: []any{},
+			want:       []any{},
 		},
 
 		{
-			name: "mixed types",
-			d:    mixed,
-			want: mixedValues,
+			name:       "mixed types",
+			initValues: []any{1, "hello", 0x10},
+			want:       []any{1, "hello", 0x10},
 		},
 
 		{
-			name: "base-case",
-			d:    basic,
-			want: values,
+			name:       "base-case",
+			initValues: []any{1, '1'},
+			want:       []any{1, '1'},
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.d.ToArray(); !reflect.DeepEqual(got, tt.want) {
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			if got := d.ToArray(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Deque.ToArray() = %v, want %v", got, tt.want)
 			}
 		})
@@ -165,28 +110,40 @@ func TestDeque_PushFront(t *testing.T) {
 	type args struct {
 		values []any
 	}
+
 	tests := []struct {
-		name string
-		d    *Deque
-		args args
-		want []any
+		name       string
+		args       args
+		initValues []any
+		want       []any
 	}{
 		{
-			name: "push one",
-			d:    New(),
+			name:       "push one to empty deque",
+			initValues: []any{},
 			args: args{
 				values: []any{1},
 			},
 			want: []any{1},
 		},
+
+		{
+			name:       "push one to full deque",
+			initValues: []any{1, true, "hello"},
+			args: args{
+				values: []any{0},
+			},
+			want: []any{0, 1, true, "hello"},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.d.PushFront(tt.args.values...)
-			got := tt.d.ToArray()
+			d := New[any]()
+			d.PushFront(tt.initValues...)
 
-			if !reflect.DeepEqual(got, tt.want) {
+			d.PushFront(tt.args.values...)
+
+			if got := d.ToArray(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Deque.PushFront(%v) got = %v, want = %v", tt.args.values, got, tt.want)
 			}
 		})
@@ -197,59 +154,71 @@ func TestDeque_PushBack(t *testing.T) {
 	type args struct {
 		values []any
 	}
+
 	tests := []struct {
-		name string
-		d    *Deque
-		args args
-		want []any
+		name       string
+		args       args
+		initValues []any
+		want       []any
 	}{
 		{
-			name: "push one",
-			d:    New(),
+			name:       "push one to empty deque",
+			initValues: []any{},
 			args: args{
 				values: []any{1},
 			},
 			want: []any{1},
 		},
+
+		{
+			name:       "push one to full deque",
+			initValues: []any{1, true, "hello"},
+			args: args{
+				values: []any{0},
+			},
+			want: []any{1, true, "hello", 0},
+		},
 	}
 
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			tt.d.PushFront(tt.args.values...)
-			got := tt.d.ToArray()
+			d := New[any]()
+			d.PushFront(tt.initValues...)
 
-			if !reflect.DeepEqual(got, tt.want) {
+			d.PushBack(tt.args.values...)
+
+			if got := d.ToArray(); !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Deque.PushBack(%v) got = %v, want = %v", tt.args.values, got, tt.want)
 			}
 		})
 	}
+
 }
 
 func TestDeque_Clear(t *testing.T) {
-	fullDeque := New()
-	values := []any{1, true, 5 * 2i, uintptr(3), "hello", ' '}
-	fullDeque.PushBack(values...)
-
 	tests := []struct {
-		name string
-		d    *Deque
-		want int
+		name       string
+		initValues []any
+		want       int
 	}{
 		{
-			name: "empty deque1",
-			d:    New(),
-			want: 0,
+			name:       "empty deque1",
+			initValues: []any{},
+			want:       0,
 		},
 
 		{
-			name: "full deque",
-			d:    fullDeque,
-			want: len(values),
+			name:       "full deque",
+			initValues: []any{1, 2, 3},
+			want:       3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			if got := tt.d.Clear(); got != tt.want || tt.d.Len() != 0 {
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			if got := d.Clear(); got != tt.want || d.list.Len() != 0 {
 				t.Errorf("Deque.Clear() = %v, want %v", got, tt.want)
 			}
 		})
@@ -257,33 +226,32 @@ func TestDeque_Clear(t *testing.T) {
 }
 
 func TestDeque_PopFront(t *testing.T) {
-	fullDeque := New()
-	values := []any{nil, true, 5 * 2i, uintptr(3), "hello", ' '}
-	fullDeque.PushBack(values...)
-
 	tests := []struct {
-		name  string
-		d     *Deque
-		want  any
-		want1 bool
+		name       string
+		initValues []any
+		want       any
+		want1      bool
 	}{
 		{
-			name:  "empty deque",
-			d:     New(),
-			want:  nil,
-			want1: false,
+			name:       "empty deque",
+			initValues: []any{},
+			want:       nil,
+			want1:      false,
 		},
 
 		{
-			name:  "full deque",
-			d:     fullDeque,
-			want:  values[0],
-			want1: true,
+			name:       "full deque",
+			initValues: []any{1, 2, 3, "hello", &list.Element{}},
+			want:       1,
+			want1:      true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := tt.d.PopFront()
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			got, got1 := d.PopFront()
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Deque.PopFront() got = %v, want %v", got, tt.want)
 			}
@@ -295,33 +263,32 @@ func TestDeque_PopFront(t *testing.T) {
 }
 
 func TestDeque_PopBack(t *testing.T) {
-	fullDeque := New()
-	values := []any{nil, true, 5 * 2i, uintptr(3), "hello", ' '}
-	fullDeque.PushBack(values...)
-
 	tests := []struct {
-		name  string
-		d     *Deque
-		want  any
-		want1 bool
+		name       string
+		initValues []any
+		want       any
+		want1      bool
 	}{
 		{
-			name:  "empty deque",
-			d:     New(),
-			want:  nil,
-			want1: false,
+			name:       "empty deque",
+			initValues: []any{},
+			want:       nil,
+			want1:      false,
 		},
 
 		{
-			name:  "full deque",
-			d:     fullDeque,
-			want:  ' ',
-			want1: true,
+			name:       "full deque",
+			initValues: []any{1, true, &testing.B{}, ' '},
+			want:       ' ',
+			want1:      true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
-			got, got1 := tt.d.PopBack()
+			d := New[any]()
+			d.PushFront(tt.initValues...)
+
+			got, got1 := d.PopBack()
 			if !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Deque.PopBack() got = %v, want %v", got, tt.want)
 			}
